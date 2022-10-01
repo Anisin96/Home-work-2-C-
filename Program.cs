7c49ee0e6823f165160edaab6e830f03c78fc581
Alexandr <anisin.broker@yandex.ru>
2022-10-01 11:06:36 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+// Console.WriteLine("Введите трехзначное число ");
+// int numder = int.Parse(Console.ReadLine());
+// int num2 = (numder/10)%10;
+// Console.WriteLine(num2);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// if (chosenNumber == 2){
+//     Console.Write("Введите число: ");
+//     string number = Console.ReadLine()!;
+//     if (number.Length < 3){
+//         Console.WriteLine($"Третьей цифры нет!");
+//     }
+//     else{
+//         Console.WriteLine($"Третья цифра числа {number} == {number[2]}");
+//     }
+// }
+
+// else if (chosenNumber < 1 || chosenNumber > 2) {
+//     Console.Write("Такого варианта нет!");
+// }
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.WriteLine("Введите число от 1 до 7");
+// int n = int.Parse(Console.ReadLine());
+// if (n < 6){
+//     Console.WriteLine($"{n} -> рабочий");
+// }
+// else{
+//      Console.WriteLine($"{n} -> выходной");
+
+// }
